cc060d080305de559b08932fa09fd816877eb8a4
Leah <105241362+Newt93@users.noreply.github.com>
2023-01-21 10:10:13 -0600

    Added multiple new security features
    Added encryption for the global settings, SSL Validation and ability to let the user upload their own SSL Certs in a separate tab, Checks the certs if they are valid, otherwise displays a message to the user to warn them or sends them back to the homepage if they choose not to continue, added a Sandbox Environment that also provides the user with the ability to not let outside influences affect their computer. (needs code optimization it's a bit messy now)

--- main.py
+++ main.py
@@ -7,25 +7,68 @@ from PyBlocking import PyBlocking
 from google.cloud import safebrowsing
 import execjs
 from concurrent.futures import ThreadPoolExecutor
+import ssl
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
+from PyQt5.QtGui import QIcon
+from cryptography.fernet import Fernet
+        
+
+
+
 
 
 app = QApplication(sys.argv)
 window = QMainWindow()
 view = QWebEngineView()
 
-from PyQt5.QtGui import QIcon
 
 class Browser(QtWebEngineWidgets.QWebEngineView):
     def __init__(self):
+        def execute_code(code):
+            # Create a new sandbox
+            sb = Sandbox()
+            # Execute the code within the sandbox
+            result = sb.execute(code)
+            # Check the result for errors
+            if result.error:
+                print(result.error)
+            else:
+                print(result.output)
+ 
         super().__init__()
         self.setWindowTitle("Atomic Browser")
         self.setWindowIcon(QIcon("path/to/logo.png"))
+        from sandbox import Sandbox
+
+
         self.ctx = execjs.compile("""
             var V8 = require("v8");
             function executeJS(javascript) {
                 return V8.eval(javascript);
             }
         """)
+
+            def encrypt_settings(self, settings_data):
+                # Serialize the settings data
+                data = json.dumps(settings_data)
+                # Encrypt the data
+                encrypted_data = self.cipher.encrypt(data.encode())
+                # Store the encrypted data
+                with open("settings.bin", "wb") as f:
+                    f.write(encrypted_data)
+
+            def decrypt_settings(self):
+                # Load the encrypted data
+                with open("settings.bin", "rb") as f:
+                    encrypted_data = f.read()
+                # Decrypt the data
+                data = self.cipher.decrypt(encrypted_data).decode()
+                # Deserialize the data
+                settings_data = json.loads(data)
+                return settings_data
+
+
         # History tab close / clear etc
         self.history_tab = QtWidgets.QTabWidget()
         self.history_tab.setTabPosition(QtWidgets.QTabWidget.West)
@@ -106,6 +149,115 @@ class HistoryTab(QtWidgets.QWidget):
         self.settings().setAttribute(QtWebEngineWidgets.QWebEngineSettings.LocalStorageEnabled, True)
         # Set the cache size
         self.profile().setHttpCacheMaximumSize(1024 * 1024 * 100) # 100MB
+        self.manager = QNetworkAccessManager()
+        self.manager.sslErrors.connect(self.handle_ssl_errors)
+        self.profile().setNetworkAccessManager(self.manager)
+        # ssl error handler
+        self.sslErrors.connect(self.sslErrorHandler)
+
+    def sslErrorHandler(self, reply, errors):
+        for error in errors:
+            # Get the certificate for the error
+            cert = error.certificate()
+            # Check the certificate's expiry date
+            if cert.expiryDate() < QDateTime.currentDateTime():
+                # Certificate has expired
+                print("Error: Certificate has expired.")
+            else:
+                # Check the certificate's subject and issuer
+                subject = cert.subjectInfo(QSslCertificate.CommonName)
+                issuer = cert.issuerInfo(QSslCertificate.CommonName)
+                if subject != "example.com" or issuer != "Example CA":
+                    # Certificate is not valid
+                    print("Error: Invalid certificate.")
+                else:
+                    # Certificate is valid
+                    print("Certificate is valid.")
+
+    def load(self, url):
+        if url.scheme() != 'https':
+            url.setScheme('https')
+        super().load(url)
+    def create_trust_store(self):
+        trust_store = set()
+        # Add root CA certificates to trust store
+        trust_store.add(x509.load_pem_x509_certificate(root_ca_pem_data, default_backend()))
+        return trust_store
+    def validate_certificate(self, certificate):
+        # Extract root CA certificate from chain
+        root_ca_cert = certificate.get_extension_for_class(x509.SubjectKeyIdentifier).value
+        # Check if root CA is in trust store
+        if root_ca_cert not in self.trust_store:
+            return False
+        return True
+    def ssl_errors(self, reply, errors):
+        for error in errors:
+            if isinstance(error, ssl.CertificateError):
+                # Extract certificate chain from error
+                certificate = error.certificate
+                if not self.validate_certificate(certificate):
+                    # Reject connection if certificate is not trusted
+                    reply.ignoreSslErrors()
+
+         # Create a tab for uploading trusted certificates
+        self.certificate_tab = QtWidgets.QWidget()
+        self.certificate_tab.setLayout(QtWidgets.QVBoxLayout())
+        self.addTab(self.certificate_tab, "Trusted Certificates")
+
+        # Create a file dialog for selecting the certificate file
+        self.file_dialog = QtWidgets.QFileDialog(self)
+        self.file_dialog.setNameFilter("Certificate files (*.crt, *.pem)")
+
+        # Create a button for opening the file dialog
+        self.upload_button = QtWidgets.QPushButton("Upload Certificate")
+        self.upload_button.clicked.connect(self.upload_certificate)
+        self.certificate_tab.layout().addWidget(self.upload_button)
+
+        # Create a list widget for displaying the uploaded certificates
+        self.certificate_list = QtWidgets.QListWidget()
+        self.certificate_tab.layout().addWidget(self.certificate_list)
+        
+        def upload_certificates(self, path):
+            try:
+                # Open the file specified by the user
+                with open(path, "rb") as cert_file:
+                    # Read the certificate data
+                    cert_data = cert_file.read()
+                    # Load the certificate using the cryptography library
+                    cert = x509.load_pem_x509_certificate(cert_data, default_backend())
+                    # Verify that the certificate is valid and has not expired
+                    cert.public_key().verify(
+                        cert.signature,
+                        cert.tbs_certificate_bytes,
+                        padding.PKCS1v15(),
+                        cert.signature_hash_algorithm
+                    )
+                    # Add the certificate to the browser's trust store
+                    self.profile().setTrustedCertificates(
+                        [cert_data],
+                        QWebEngineClientCertificateStore.Pem
+                    )
+                    print("Certificate uploaded successfully")
+            except Exception as e:
+                def check_certificate(self, url):
+                    certificate = self.get_certificate(url)
+                    if not certificate.is_valid():
+                        msg = QMessageBox()
+                        msg.setIcon(QMessageBox.Warning)
+                        msg.setText("Invalid certificate detected")
+                        msg.setInformativeText("The website's certificate is not valid. Do you want to continue anyway?")
+                        msg.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+                        msg.setDefaultButton(QMessageBox.No)
+                        retval = msg.exec_()
+                        if retval == QMessageBox.Yes:
+                            self.load(QUrl(url))
+                        else:
+                            self.load(QUrl("https://www.google.com"))
+                    else:
+                        self.load(QUrl(url))
+                            browser = Browser()
+                            browser.upload_certificates("path/to/certificate.pem")
+
         # Initialize SafeBrowsing
         self.safe_browsing = SafeBrowsing("API_KEY")
     # Use JV8 to load JS (Makes browser faster)
@@ -187,15 +339,53 @@ add_bookmark_action = QAction("Add Bookmark", view)
 add_bookmark_action.triggered.connect(add_bookmark)
 window.addAction(add_bookmark_action)
 
+class GlobalSettings:
+    def __init__(self):
+        self.key = Fernet.generate_key()
+        self.cipher = Fernet(self.key)
+        settings = QWebEngineSettings.globalSettings()
+        settings.setAttribute(QWebEngineSettings.AutoLoadIconsForPage, True)
+        settings.setAttribute(QWebEngineSettings.JavascriptEnabled, True)
+        settings.setAttribute(QWebEngineSettings.JavascriptCanOpenWindows, True)
+        settings.setAttribute(QWebEngineSettings.JavascriptCanAccessClipboard, True)
+        settings.setAttribute(QWebEngineSettings.LocalStorageEnabled, True)
+        settings.setAttribute(QWebEngineSettings.XSSAuditingEnabled, True)
+        settings.setAttribute(QWebEngineSettings.SpatialNavigationEnabled, True)
+        settings.setAttribute(QWebEngineSettings.LocalContentCanAccessRemoteUrls, True)
+        settings.setAttribute(QWebEngineSettings.HyperlinkAuditingEnabled, True)
+        settings.setAttribute(QWebEngineSettings.FullScreenSupportEnabled, True)
+        settings.setAttribute(QWebEngineSettings.ScreenCaptureEnabled, True)
+        settings.setAttribute(QWebEngineSettings.WebGLEnabled, True)
+        settings.setAttribute(QWebEngineSettings.Accelerated2dCanvasEnabled, True)
+        settings.setAttribute(QWebEngineSettings.AutoLoadIconsForPage, True)
+        settings.setAttribute(QWebEngineSettings.JavascriptCanCloseWindows, True)
+        settings.setAttribute(QWebEngineSettings.ErrorPageEnabled, True)
+        settings.setAttribute(QWebEngineSettings.PluginsEnabled, True)
+        settings.setAttribute(QWebEngineSettings.ShowScrollBars, True)
+        settings.setAttribute(QWebEngineSettings.WebSecurityEnabled, True)
+        settings.setAttribute(QWebEngineSettings.LocalContentCanAccessFileUrls, True)
+        settings.setAttribute(QWebEngineSettings.DnsPrefetchEnabled, True)
+        settings.setAttribute(QWebEngineSettings.PlaybackRequiresUserGesture, True)
+
 
 if __name__ == "__main__":
-    import sys
+    # Create an instance of the Browser class
+    browser = Browser()
+    # Encrypt the global settings using the cryptography library
+    encrypt_settings(browser.settings)
+    # Create a sandbox environment using the python-sandbox library
+    create_sandbox(browser)
+
+    # Initialize the SSL/TLS certificate validation and pinning
+    ssl_context = ssl.create_default_context(purpose=ssl.Purpose.SERVER_AUTH, cafile='path/to/trusted_certificates.pem')
+    ssl_context.check_hostname = False
+    ssl_context.verify_mode = ssl.CERT_REQUIRED
+    ssl_context.load_verify_locations('path/to/trusted_certificates.pem')
+    ssl_context.set_ciphers('ECDHE-ECDSA-AES256-GCM-SHA384')
+    QSslConfiguration.setDefaultConfiguration(ssl_context)
 
     app = QtWidgets.QApplication(sys.argv)
     browser = Browser("YOUR_API_KEY")
     browser.show()
     browser.load_urls(["https://www.google.com"])
     sys.exit(app.exec_())
-
-
-
